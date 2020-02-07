68aba72343cbcdf9ad2bf0fe7c3c40422e12210e
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-07 10:47:15 -0700

    Create project show page test

---
+++ spec/features/user_can_see_project_info_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit a project page', type: :feature do
+  it 'I can see project info' do
+    challenge1 = Challenge.create!(theme: 'Apartment Furnishings',
+                                   project_budget: 1000)
+    project1 = Project.create!(name: 'Litfit',
+                               material: 'Lamp Shade',
+                               challenge_id: challenge1)
+    visit "/projects/:id"
+
+    expect(page).to have_content(project1.name)
+    expect(page).to have_content("Material: #{project1.material}")
+    expect(page).to have_content("Challenge Theme: #{challenge1.theme}")
+
+  end
+
+end
